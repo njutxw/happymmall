6333ff9dc8bd6ab8b52f3cfea9644ddf94416814
njutxw <1352375760@qq.com>
2020-02-03 18:39:05 +0800

    2020/2/3 commit

---
+++ src/main/java/com/mmall/service/IFileService.java
@@ -0,0 +1,56 @@
+package com.mmall.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface IFileService {
+
+    String upload(MultipartFile file, String path);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
